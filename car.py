8e6f3f96f836329120b4a2466e04d65323a7cb5c
Gal <57294938+ProjectSciFy@users.noreply.github.com>
2022-10-08 12:05:35 -0400

    Update car.py

--- car.py
+++ car.py
@@ -1,38 +1,29 @@
 class Car:
     def __init__(self, location, speed, direction, size):
-        self.location = location
-        self.speed = speed
-        self.direction = direction
         self.size = size
         self.context = None
-        self.state = (self.location, self.speed, self.direction)
-        self.speedLimit = 50
+        self.state = (location, speed, direction)
         self.start = (0, 0)
         self.goal = (800,600)
         self.inTransition = False
-        self.neighbors = [[False, False, False], [True, self, True], [False, False, False]]
-        self.context = (self.speedLimit, self.inTransition, self.neighbors)
+        self.context = (50, self.inTransition, [[False, False, False], [True, self, True], [False, False, False]])
         
     def getState(self):
         return self.state
     
     def setState(self, location, speed, direction, size):
-        self.location = location
-        self.speed = speed
-        self.direction = direction
         self.size = size
-        self.state = (self.location, self.speed, self.direction)
+        self.state = (location, speed, direction)
     
     def getContext(self):
         return self.context
         
-    def setContext(self, speedLimit, start, goal, neighbors):
-        self.speedLimit = speedLimit
+    def setContext(self, speedLimit, neighbors):
+        self.context = (speedLimit, self.inTransition, neighbors)
+        
+    def getRoute(self):
+        return (self.start, self.goal)
+    
+    def setRoute(self, start, goal):
         self.start = start
         self.goal = goal
-        self.inTransition = False
-        self.neighbors = neighbors
-        self.context = (self.speedLimit, self.inTransition, self.neighbors)
-        
-    def getSpeedLimit(self):
-        return self.speedLimit
