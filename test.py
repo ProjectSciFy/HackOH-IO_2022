5f3d48eab0b261f5868ce72fc7580ed598706ea4
Gal <57294938+ProjectSciFy@users.noreply.github.com>
2022-10-08 14:42:53 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
-import car
+import car as CAR
+import os
 
+
+def runCar(car):
+    car.setRoute((0, 300))
+    car.distAlgo()
+
+num = input("Number of cars: \n")
 location = (800, 300)
 speed = 50
 direction = "W"
 size = (40, 20)
-first = car.Car(location, speed, direction, size)
-first.setRoute((0, 300))
-first.distAlgo()
-print("end")
-print(first.getState())
+cars = list()
+amFather = False
+children = []
+for i in range(int(num)):
+    cars.append(CAR.Car((location[0] - (int(i) * 50), location[1]), speed, direction, size))
+    print(f"\tStart of car {i + 1}: ", cars[i].getState())
+
+for car in cars:
+    pid = os.fork()
+    if pid > 0:
+        amFather = True
+    else:
+        children.append(pid)
+        runCar(car)
+        print(f"\tEnd state of a car: ", car.getState())
+        exit(0)
+        
+
+if amFather:
+    for child in children:
+        os.waitpid(child, 0)
+        
