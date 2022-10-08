af3ef4cdecb1c8e0f027084697be7c364d1e5906
matthewyfong <matthewykfong@gmail.com>
2022-10-08 11:25:53 -0400

    init car

---
+++ car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, location, speed, direction, size):
+        self.location = location
+        self.speed = speed
+        self.direction = direction
+        self.size = size
+
