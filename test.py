219d88bb04722b4eb5b5e759bbcc04802a886bc9
Gal <57294938+ProjectSciFy@users.noreply.github.com>
2022-10-09 00:46:04 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ import random
 def setup():
     with open("1c_1l.txt") as f:
         input = f.readlines()
-    num = input[0]
-    lanes = input[1]
+    num = int(input[0])
+    lanes = int(input[1])
     location = (800, 300)
     speed = 20
     direction = "W"
     size = (20, 20)
-    lane_left_to_right = (30, 315 + 30 * int(lanes), 285 + 30 * int(lanes))
+    lane_left_to_right = (30, 315 + 30 * (int(lanes) - 1), 285 - 30 * (int(lanes) - 1))
     cars = list()
     temp = 0
-    for i in range(int(num)):
+    for i in range(num):
         lane = random.randrange(int(lanes))
         tempCar = CAR.Car((location[0], location[1] - (lane * 30)), speed, direction, lane, size, i, lanes, lane_left_to_right)
         tempCar.setRoute((0, 300 - lane_left_to_right[0] * lane))
@@ -23,19 +23,24 @@ def setup():
         temp = i
     temp += 1
     # car moving other way:
-    # tCar = CAR.Car((0, 300), speed, "E", size, temp)
-    # tCar.setRoute((800, 300))
-    # cars.append(tCar)
-    # tCar.addCarToAllCars(tCar)
-    # print(f"\tStart of car {temp}: ", cars[temp].getState())
+    tCar = CAR.Car((500, 300), 0, "E", 0, size, temp, lanes, lane_left_to_right)
+    tCar.setRoute((800, 300))
+    cars.append(tCar)
+    tCar.addCarToAllCars(tCar)
+    print(f"\tStart of car {temp}: ", cars[temp].getState())
     updateCars(cars, lanes)
 
 def updateCars(cars, lanes):
-    while len(CAR.Car.allCars) > 0:
+    iter = 0
+    while len(CAR.Car.allCars) > 0 and iter < 50:
         for car in cars:
             temp = car.distAlgo("W", 300, 300, lanes)
             if temp == 69:
                 print(f"\tEnd of car {car.key}: ", car.getState())
                 break
+            elif temp == 10:
+                print(f"\tCar {car.key} detected a stopped object on road and stopped at: ", car.getState())
+                exit(1)
+        iter += 1
             
 setup()
