37291a7d87280cfc4611dfaaf60517aa322c33ca
Gal <57294938+ProjectSciFy@users.noreply.github.com>
2022-10-09 09:22:11 -0400

    Update sim.py

--- sim.py
+++ sim.py
@@ -108,9 +108,119 @@ refresh_rate = 60
 simulation_over = False
 
 
+###############
+# SIMULATIONS #
+###############
 
-def setup():
-    with open("1c_1l_N.txt") as f:
+def setup1():
+    with open("1c_1l_W.txt") as f:
+        input = f.readlines()
+    num = int(input[0])
+    lanes = int(input[1])
+    direction1 = str(input[2])
+    location = (800, 300)
+    if direction1 == "N":
+        location = (400, 600)
+    speed = 3
+    size = (20, 20)
+    lane_left_to_right = (30, 315 + 30 * (int(lanes) - 1), 285 - 30 * (int(lanes) - 1))
+    cars = list()
+    for i in range(num):
+        lane = random.randrange(int(lanes))
+        tempCar = CAR.Car((location[0], location[1] - i * 180), speed, direction1, lane, size, i, lanes, lane_left_to_right)
+        if direction1 == "W":
+            tempCar.setRoute((0, 300 - lane_left_to_right[0] * lane))
+        if direction1 == "N":
+            tempCar.setRoute((400 + lane_left_to_right[0] * lane, 0))
+        cars.append(tempCar)
+        tempCar.addCarToAllCars(tempCar)
+        print(f"\tStart of car {i} heading {direction1}: ", cars[i].getState())
+    create_roads('horizontal', int(lanes), location[0], location[1] + size[1] / 2, 40)  
+    updateCars(cars, lanes)
+
+
+def setup2():
+    with open("1c_2l_W.txt") as f:
+        input = f.readlines()
+    num = int(input[0])
+    lanes = int(input[1])
+    direction1 = str(input[2])
+    location = (800, 300)
+    if direction1 == "N":
+        location = (400, 600)
+    speed = 6
+    size = (20, 20)
+    lane_left_to_right = (30, 315 + 30 * (int(lanes) - 1), 285 - 30 * (int(lanes) - 1))
+    cars = list()
+    for i in range(num):
+        lane = random.randrange(int(lanes))
+        tempCar = CAR.Car((location[0], location[1]), int(speed/2), direction1, lane, size, i, lanes, lane_left_to_right)
+        if direction1 == "W":
+            tempCar.setRoute((0, 300 - lane_left_to_right[0] * lane))
+        if direction1 == "N":
+            tempCar.setRoute((400 + lane_left_to_right[0] * lane, 0))
+        cars.append(tempCar)
+        tempCar.addCarToAllCars(tempCar)
+        print(f"\tStart of car {i} heading {direction1}: ", cars[i].getState())
+    create_roads('horizontal', int(lanes), location[0], location[1] - size[1] / 2, 40)  
+    updateCars(cars, lanes)
+
+
+def setup3():
+    with open("3c_3l_W.txt") as f:
+        input = f.readlines()
+    num = int(input[0])
+    lanes = int(input[1])
+    direction1 = str(input[2])
+    location = (800, 300)
+    if direction1 == "N":
+        location = (400, 600)
+    speed = 6
+    size = (20, 20)
+    lane_left_to_right = (30, 315 + 30 * (int(lanes) - 1), 285 - 30 * (int(lanes) - 1))
+    cars = list()
+    for i in range(num):
+        lane = random.randrange(int(lanes))
+        tempCar = CAR.Car((location[0] + (i * 80), location[1] - lane_left_to_right[0] * lane - lane * 10), random.randint(2, int(speed/2)), direction1, lane, size, i, lanes, lane_left_to_right)
+        if direction1 == "W":
+            tempCar.setRoute((0, 300 - lane_left_to_right[0] * lane))
+        if direction1 == "N":
+            tempCar.setRoute((400 + lane_left_to_right[0] * lane, 0))
+        cars.append(tempCar)
+        tempCar.addCarToAllCars(tempCar)
+        print(f"\tStart of car {i} heading {direction1} in lane {lane}: ", cars[i].getState())
+    create_roads('horizontal', int(lanes), location[0], location[1] - 3 * size[1] / 2, 40)  
+    updateCars(cars, lanes)
+    
+def setup4():
+    with open("10c_3l_W.txt") as f:
+        input = f.readlines()
+    num = int(input[0])
+    lanes = int(input[1])
+    direction1 = str(input[2])
+    location = (800, 300)
+    if direction1 == "N":
+        location = (400, 600)
+    speed = 6
+    size = (20, 20)
+    lane_left_to_right = (30, 315 + 30 * (int(lanes) - 1), 285 - 30 * (int(lanes) - 1))
+    cars = list()
+    for i in range(num):
+        lane = random.randrange(int(lanes))
+        tempCar = CAR.Car((location[0] + (i * 110), location[1] - lane_left_to_right[0] * lane - lane * 10), random.randint(2, int(speed/2)), direction1, lane, size, i, lanes, lane_left_to_right)
+        if direction1 == "W":
+            tempCar.setRoute((0, 300 - lane_left_to_right[0] * lane - lane * 10))
+        if direction1 == "N":
+            tempCar.setRoute((400 + lane_left_to_right[0] * lane, 0))
+        cars.append(tempCar)
+        tempCar.addCarToAllCars(tempCar)
+        print(f"\tStart of car {i} heading {direction1} in lane {lane}: ", cars[i].getState())
+    create_roads('horizontal', int(lanes), location[0], location[1] - 3 * size[1] / 2, 40)  
+    updateCars(cars, lanes)
+
+
+def setup5():
+    with open("2c_1l_N.txt") as f:
         input = f.readlines()
     num = int(input[0])
     lanes = int(input[1])
@@ -132,6 +242,9 @@ def setup():
         cars.append(tempCar)
         tempCar.addCarToAllCars(tempCar)
         print(f"\tStart of car {i} heading {direction1}: ", cars[i].getState())
+    
+    create_roads('vertical', int(lanes), 400 + size[1]/2, location[1] + size[1] / 2, 40)    
+    
     temp = num
     # car stopped in West lane:
     tCar = CAR.Car((100, 300), 0, "W", 0, size, temp, lanes, lane_left_to_right)
@@ -140,7 +253,7 @@ def setup():
     tCar.addCarToAllCars(tCar)
     print(f"\tStart of car {temp} heading W: ", cars[temp].getState())
 
-    with open("1c_1l_W.txt") as f:
+    with open("3c_1l_W.txt") as f:
         input = f.readlines()
     num = int(input[0])
     lanes = int(input[1])
@@ -160,8 +273,7 @@ def setup():
         tempCar.addCarToAllCars(tempCar)
         print(f"\tStart of car {i} heading {direction2}: ", cars[i].getState())
         i += 1
-    create_roads('horizotal', int(lanes), location[0], location[1] + size[1] / 2, 40)
-    create_roads('vertical', int(lanes), 400 + size[1]/2, location[1] + size[1] / 2, 40)
+    create_roads('horizontal', int(lanes), location[0], location[1] + size[1] / 2, 40)
 
     updateCars(cars, lanes)
 
@@ -177,11 +289,9 @@ def updateCars(cars, lanes):
             #     print(f"\tCar {car.key} detected a stopped object on road and stopped at: ", car.getState())
             #     break
         iter += 1
-
-
+        
         # while not simulation_over:
         #simulation_over = check_input(pygame.event.get())
-
         clear_board()
         update(cars)
         draw_roads(roads)
@@ -195,5 +305,8 @@ def updateCars(cars, lanes):
     pygame.quit()
     quit()
 
-
-setup()
+# setup1()
+# setup2()
+# setup3()
+# setup4()
+setup5()
